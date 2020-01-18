006b0ff61748d323ac801c7d8855abd941bcb250
Zhan Poplavskyi <gan.popl@gmail.com>
2020-01-19 00:22:49 +0200

    Refactoring/optimizations

--- src/game.rs
+++ src/game.rs
@@ -5,6 +5,7 @@ use piston_window::*;
 use crate::level::Level;
 
 pub struct Game {
+    game_zone: GameZone,
     level: Level,
     prev_time: f64,
     game_over: bool,
@@ -13,6 +14,7 @@ pub struct Game {
 impl Game {
     pub fn new() -> Self {
         Game {
+            game_zone: GameZone::new(10.0, 30.0, 202.0, 402.0),
             level: Level::new(),
             prev_time: 0_f64,
             game_over: false,
@@ -38,42 +40,107 @@ impl Game {
     }
 
     pub fn render(&self, window: &mut PistonWindow, e: &Event) {
+        let window_settings = window.window.size();
         window.draw_2d(e, |c, g, _device| {
             clear([0.0; 4], g);
 
             /* window 640x480 */
-            line([1.0; 4], 1.0, [0.0, 0.0, 640.0, 0.0], c.transform, g);
-            line([1.0; 4], 1.0, [640.0, 0.0, 640.0, 480.0], c.transform, g);
-            line([1.0; 4], 1.0, [640.0, 480.0, 0.0, 480.0], c.transform, g);
-            line([1.0; 4], 1.0, [0.0, 0.0, 0.0, 480.0], c.transform, g);
+            line(
+                [1.0; 4],
+                1.0,
+                [0.0, 0.0, window_settings.width, 0.0],
+                c.transform,
+                g,
+            );
+            line(
+                [1.0; 4],
+                1.0,
+                [
+                    window_settings.width,
+                    0.0,
+                    window_settings.width,
+                    window_settings.height,
+                ],
+                c.transform,
+                g,
+            );
+            line(
+                [1.0; 4],
+                1.0,
+                [
+                    window_settings.width,
+                    window_settings.height,
+                    0.0,
+                    window_settings.height,
+                ],
+                c.transform,
+                g,
+            );
+            line(
+                [1.0; 4],
+                1.0,
+                [0.0, 0.0, 0.0, window_settings.height],
+                c.transform,
+                g,
+            );
             /* window end */
 
             if !self.game_over {
                 /* game field */
-                let x1 = 10.0;
-                let y1 = 10.0;
-                line([1.0; 4], 1.0, [x1, y1, x1 + 202.0, y1], c.transform, g);
                 line(
                     [1.0; 4],
                     1.0,
-                    [x1 + 202.0, y1, x1 + 202.0, y1 + 402.0],
+                    [
+                        self.game_zone.x,
+                        self.game_zone.y,
+                        self.game_zone.x + self.game_zone.width,
+                        self.game_zone.y,
+                    ],
                     c.transform,
                     g,
                 );
                 line(
                     [1.0; 4],
                     1.0,
-                    [x1 + 202.0, y1 + 402.0, x1, y1 + 402.0],
+                    [
+                        self.game_zone.x + self.game_zone.width,
+                        self.game_zone.y,
+                        self.game_zone.x + self.game_zone.width,
+                        self.game_zone.y + self.game_zone.height,
+                    ],
                     c.transform,
                     g,
                 );
-                line([1.0; 4], 1.0, [x1, y1, x1, y1 + 402.0], c.transform, g);
+                line(
+                    [1.0; 4],
+                    1.0,
+                    [
+                        self.game_zone.x + self.game_zone.width,
+                        self.game_zone.y + self.game_zone.height,
+                        self.game_zone.x,
+                        self.game_zone.y + self.game_zone.height,
+                    ],
+                    c.transform,
+                    g,
+                );
+                line(
+                    [1.0; 4],
+                    1.0,
+                    [
+                        self.game_zone.x,
+                        self.game_zone.y,
+                        self.game_zone.x,
+                        self.game_zone.y + self.game_zone.height,
+                    ],
+                    c.transform,
+                    g,
+                );
 
                 let square_side = 20_f64; //px
                 for i in 0..self.level.width() {
                     for j in 0..self.level.height() {
-                        let x2 = i as f64 * square_side + x1 + 1.0;
-                        let y2 = j as f64 * square_side + y1 + 1.0;
+                        let x2 = i as f64 * square_side + self.game_zone.x + 1.0;
+                        let y2 = j as f64 * square_side + self.game_zone.y + 1.0;
                         if self.level.get(i, j) {
                             rectangle(
                                 [0.86, 0.86, 0.86, 1.0],
@@ -94,7 +161,17 @@ impl Game {
                 }
             /* game field end */
             } else {
-                rectangle([1.0; 4], [10.0, 10.0, 202.0, 402.0], c.transform, g);
+                rectangle(
+                    [1.0; 4],
+                    [
+                        self.game_zone.x,
+                        self.game_zone.y,
+                        self.game_zone.width,
+                        self.game_zone.height,
+                    ],
+                    c.transform,
+                    g,
+                );
             }
         });
     }
@@ -107,6 +184,24 @@ impl Game {
             } else {
                 self.game_over = true;
             }
+        }
+    }
+}
+
+struct GameZone {
+    pub x: f64,
+    pub y: f64,
+    pub width: f64,
+    pub height: f64,
+}
+
+impl GameZone {
+    fn new(x: f64, y: f64, width: f64, height: f64) -> Self {
+        GameZone {
+            x,
+            y,
+            width,
+            height,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     let settings = Settings::new();
     let mut game = Game::new();
     let mut window: PistonWindow = WindowSettings::new(
-        "Tetris v0.1 pre-alpha",
+        "Tetris v0.1.1 pre-alpha",
         [settings.game_width, settings.game_height],
     )
     .exit_on_esc(true)
@@ -27,7 +27,7 @@ fn main() {
 
         if let Some(args) = e.update_args() {
             game.update(args);
-        }
+        };
 
         game.render(&mut window, &e);
     }
